d2bd04e8656b894cde3c9d51670c13d9268e52b7
Moritz Ulrich <ulrich.moritz@googlemail.com>
2013-01-12 01:34:06 +0100

    handle buffer-open events

--- weechat.el
+++ weechat.el
@@ -4,17 +4,34 @@
 
 (defvar weechat--buffer-alist '())
 
+(defun weechat--add-buffer-to-alist (ptr val)
+  (setq weechat--buffer-alist
+        (cons (cons ptr val) weechat--buffer-alist)))
+
 (defun weechat--handle-buffer-list (hdata)
   (setq weechat--buffer-alist ())
   (dolist (value (weechat--hdata-values hdata))
     (let ((buffer-ptr (car (weechat--hdata-value-pointer-path value))))
-      (setq weechat--buffer-alist
-            (cons (cons buffer-ptr (weechat--hdata-value-alist value)) weechat--buffer-alist)))))
+      (weechat--add-buffer-to-alist
+       buffer-ptr
+       (weechat--hdata-value-alist value)))))
+
+(defun weechat-buffer-alist (buffer-ptr)
+  (cdr (assoc buffer-ptr weechat--buffer-alist)))
 
 (defun weechat-update-buffer-list ()
   (weechat-relay-send-command
    "hdata buffer:gui_buffers(*) name,short_name,title"
    #'weechat--handle-buffer-list))
+
+(defun weechat--handle-buffer-opened (hdata)
+  (let* ((value (car (weechat--hdata-values hdata)))
+         (buffer-ptr (car (weechat--hdata-value-pointer-path value))))
+    (when (weechat-buffer-alist buffer-ptr)
+      (error "Received '_buffer_opened' event for '%s' but the buffer exists already!" buffer-ptr))
+    (weechat--add-buffer-to-alist buffer-ptr (weechat--hdata-value-alist value))))
+
+(weechat-relay-add-id-callback "_buffer_opened" #'weechat--handle-buffer-opened nil 'force)
 
 (defun weechat-connect (host port password)
   (interactive (list (read-string "Relay Host: ")
