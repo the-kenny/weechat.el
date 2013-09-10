ecb3dc2b99f4bd00d2cdd17bc432fcecc5b4666c
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-11 01:13:54 +0200

    weechat-awareness: Add keybindings & keep point
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- weechat-awareness.el
+++ weechat-awareness.el
@@ -27,43 +27,71 @@
 
 (require 'weechat)
 
+(defvar weechat-awareness-buffer-name "*weechat-awareness*")
+
 (defvar weechat-awareness-max-column-width 18)
 
-(let* ((weechat-buffers (weechat-buffer-list))
-       (columns (/ (window-width)
-                   (+ weechat-awareness-max-column-width 3)))
-       (rows (ceiling
-              (/ (length weechat-buffers)
-                 (float columns)))))
-  (with-current-buffer (get-buffer "tabletest")
-    (let ((inhibit-read-only t))
-      (kill-region (point-min) (point-max)))
-    (goto-char (point-min))
-    (org-table-create (format "%ix%i" columns rows))
-    (org-table-begin)
-    (next-line)
-    (kill-region (point-at-bol) (1+ (point-at-eol)))
-    (org-table-begin)
-    (cl-dotimes (x columns)
-      (cl-dotimes (y rows)
-        (let ((buffer (nth (+ (* y columns) x)
-                           weechat-buffers)))
-          (when (bufferp buffer)
-            (org-table-put
-             (1+ y) (1+ x)
-             (s-pad-right
-              weechat-awareness-max-column-width " "
-              (s-prepend " "
-                         (weechat-canonical-buffer-name
-                          (buffer-local-value
-                           'weechat-buffer-ptr buffer)))))))))
-    ;; Fill the last cells in the table
-    (when (< (length weechat-buffers) (* columns rows))
-      (cl-dotimes (i (- (* columns rows) (length weechat-buffers)))
-        (org-table-put rows (- columns i)
-                       (s-pad-right weechat-awareness-max-column-width " " ""))))
-    (add-text-properties (point-min) (point-max)
-                         '(read-only t))))
+(defun weechat-awareness-update-buffer (buffer)
+  (with-current-buffer buffer
+   (let* ((pos (point))
+          (weechat-buffers (weechat-buffer-list))
+          (columns (/ (window-width)
+                      (+ weechat-awareness-max-column-width 3)))
+          (rows (ceiling
+                 (/ (length weechat-buffers)
+                    (float columns)))))
+     (let ((inhibit-read-only t))
+       (kill-region (point-min) (point-max))
+       (goto-char (point-min))
+       (org-table-create (format "%ix%i" columns rows))
+       (org-table-begin)
+       (next-line)
+       (kill-region (point-at-bol) (1+ (point-at-eol)))
+       (org-table-begin)
+       (cl-dotimes (x columns)
+         (cl-dotimes (y rows)
+           (let ((buffer (nth (+ (* y columns) x)
+                              weechat-buffers)))
+             (when (bufferp buffer)
+               (org-table-put
+                (1+ y) (1+ x)
+                (s-pad-right
+                 weechat-awareness-max-column-width " "
+                 (s-prepend " "
+                            (weechat-canonical-buffer-name
+                             (buffer-local-value
+                              'weechat-buffer-ptr buffer)))))))))
+       ;; Fill the last cells in the table
+       (when (< (length weechat-buffers) (* columns rows))
+         (cl-dotimes (i (- (* columns rows) (length weechat-buffers)))
+           (org-table-put rows (- columns i)
+                          (s-pad-right weechat-awareness-max-column-width
+                                       " " "")))))
+     (set-window-point (get-buffer-window buffer) pos))))
+
+(defvar weechat-awareness-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "RET") 'weechat-awareness-goto)
+    (define-key map (kbd "p")   'weechat-awareness-prev)
+    (define-key map (kbd "n")   'weechat-awareness-next)
+    (define-key map (kbd "TAB") 'weechat-awareness-tab)
+    (define-key map (kbd "r")   'weechat-awareness-reload)
+    map)
+  "Keymap for weechat.el's awareness plugin.")
+
+(defun weechat-awareness-mode ()
+  (setq major-mode 'weechat-awareness
+        mode-name "Weechat-Awareness")
+  (use-local-map weechat-awareness-mode-map)
+  (read-only-mode 1))
+
+(defun weechat-awareness ()
+  (interactive)
+  (let ((buffer (get-buffer-create
+                 weechat-awareness-buffer-name)))
+    (with-current-buffer buffer
+     (weechat-awareness-mode)
+     (weechat-awareness-update-buffer buffer))))
 
 (provide 'weechat-awareness)
 
