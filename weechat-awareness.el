ec7b5b1273847730eb2b57f9ac32841179fdeacd
Moritz Ulrich <moritz@tarn-vedra.de>
2013-09-11 14:40:08 +0200

    Add unread-indicator-stub, improve formatting code.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

--- weechat-awareness.el
+++ weechat-awareness.el
@@ -29,45 +29,72 @@
 
 (defvar weechat-awareness-buffer-name "*weechat-awareness*")
 
-(defvar weechat-awareness-max-column-width 18)
+(setq weechat-awareness-name-length 16)
+
+(defvar weechat-awareness-show-unread t)
+
+(defun weechat-awareness-indicator-string (buffer-ptr)
+  (if (= 0 (random 2))
+      (format "(%i)" (random 42))
+    (format "(%i,%i)" (random 42) (random 42))))
+
+(defun weechat-awareness-make-cell (buffer-ptr pad)
+  (let ((bname (s-truncate weechat-awareness-name-length
+                           (weechat-canonical-buffer-name buffer-ptr)))
+        (indicator (if weechat-awareness-show-unread
+                       (weechat-awareness-indicator-string buffer-ptr)
+                     "")))
+    (s-pad-right
+     pad
+     " "
+     (format " %s%s%s" bname (make-string (- pad
+                                             (length bname)
+                                             (length indicator)
+                                             2)
+                                          32)
+             indicator))))
 
 (defun weechat-awareness-update-buffer (buffer)
   (with-current-buffer buffer
-   (let* ((pos (point))
-          (weechat-buffers (weechat-buffer-list))
-          (columns (/ (window-width)
-                      (+ weechat-awareness-max-column-width 3)))
-          (rows (ceiling
-                 (/ (length weechat-buffers)
-                    (float columns)))))
-     (let ((inhibit-read-only t))
-       (kill-region (point-min) (point-max))
-       (goto-char (point-min))
-       (org-table-create (format "%ix%i" columns rows))
-       (org-table-begin)
-       (next-line)
-       (kill-region (point-at-bol) (1+ (point-at-eol)))
-       (org-table-begin)
-       (cl-dotimes (x columns)
-         (cl-dotimes (y rows)
-           (let ((buffer (nth (+ (* y columns) x)
-                              weechat-buffers)))
-             (when (bufferp buffer)
-               (org-table-put
-                (1+ y) (1+ x)
-                (s-pad-right
-                 weechat-awareness-max-column-width " "
-                 (s-prepend " "
-                            (weechat-canonical-buffer-name
-                             (buffer-local-value
-                              'weechat-buffer-ptr buffer)))))))))
-       ;; Fill the last cells in the table
-       (when (< (length weechat-buffers) (* columns rows))
-         (cl-dotimes (i (- (* columns rows) (length weechat-buffers)))
-           (org-table-put rows (- columns i)
-                          (s-pad-right weechat-awareness-max-column-width
-                                       " " "")))))
-     (set-window-point (get-buffer-window buffer) pos))))
+    (let* ((pos (point))
+           (weechat-buffers (weechat-buffer-list))
+           (weechat-awareness-max-column-width (+ weechat-awareness-name-length
+                                                  (if weechat-awareness-show-unread
+                                                      6 0)
+                                                  2))
+           (_ (message "%S" weechat-awareness-max-column-width))
+           (columns (/ (window-width)
+                       (+ 2 weechat-awareness-max-column-width)))
+           (rows (ceiling
+                  (/ (length weechat-buffers)
+                     (float columns)))))
+      (let ((inhibit-read-only t))
+        (kill-region (point-min) (point-max))
+        (goto-char (point-min))
+        (org-table-create (format "%ix%i" columns rows))
+        (org-table-begin)
+        (when (> rows 1)
+         (next-line)
+         (kill-region (point-at-bol) (1+ (point-at-eol)))
+         (org-table-begin))
+        (cl-dotimes (x columns)
+          (cl-dotimes (y rows)
+            (let ((buffer (nth (+ (* y columns) x)
+                               weechat-buffers)))
+              (when (bufferp buffer)
+                (let ((buffer-ptr (buffer-local-value
+                                   'weechat-buffer-ptr buffer)))
+                  (org-table-put
+                   (1+ y) (1+ x)
+                   (weechat-awareness-make-cell buffer-ptr weechat-awareness-max-column-width)))))))
+        ;; Fill the last cells in the table
+        (when (< (length weechat-buffers) (* columns rows))
+          (cl-dotimes (i (- (* columns rows) (length weechat-buffers)))
+            (org-table-put rows (- columns i)
+                           (s-pad-right weechat-awareness-max-column-width
+                                        " " "")))))
+      (when (window-live-p (get-buffer-window buffer))
+        (set-window-point (get-buffer-window buffer) pos)))))
 
 (defvar weechat-awareness-mode-map
   (let ((map (make-sparse-keymap)))
