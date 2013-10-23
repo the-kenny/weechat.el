cf0eb6b28cef1f30b046b9dff6dbe4777ea6e14e
Moritz Ulrich <moritz@tarn-vedra.de>
2013-10-24 01:19:11 +0200

    New module: weechat-alias. Define local command aliases in elisp.
    Signed-off-by: Moritz Ulrich <moritz@tarn-vedra.de>

---
+++ weechat-alias.el
@@ -0,0 +1,79 @@
+;;; weechat-alias.el --- Define local weechat commands in elisp
+
+;; Copyright (C) 2013 Moritz Ulrich <moritz@tarn-vedra.de>
+
+;; Author: Moritz Ulrich <moritz@tarn-vedra.de>
+;;         Rüdiger Sonderfeld <ruediger@c-plusplus.de>
+;;         Aristid Breitkreuz <aristidb@gmail.com>
+;; Keywords: irc chat network weechat
+;; URL: https://github.com/the-kenny/weechat.el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;; 
+;; This modules allows the user to write emacs lisp functions which
+;; are callable in weechat buffers via normal irc /command syntax.
+;; 
+;; Function names must start with `weechat-alias-function-prefix' and
+;; take one argument, the input of the user. 
+;;
+;; They might return either a string or nil. In case of string the
+;; value will be sent to the server. If the return value is nil, the
+;; command will be discarded. This is useful if you want to execute
+;; local code but not send anything to the server.
+
+;;; Code:
+
+(require 'weechat)
+(require 's)
+(require 'cl-lib)
+
+(defconst weechat-alias-function-prefix "weechat-alias-command-")
+
+(defcustom weechat-alias-command-prefix "/"
+  "Prefix used for alias commands."
+  :type 'string
+  :group 'weechat-alias)
+
+(defun weechat-alias-find-aliases ()
+  (cl-remove-if-not
+   (lambda (s)
+     (s-prefix? weechat-alias-function-prefix (symbol-name s)))
+   obarray))
+
+(defun weechat-alias-command-yay (input)
+  "http://tarn-vedra.de/pics/yay.gif")
+
+(defun weechat-alias-apply (input)
+  (if (s-prefix? weechat-alias-command-prefix input)
+      ;; Try to search for an alias command
+      (let* ((command (car (s-split-words
+                            (s-chop-prefix
+                             weechat-alias-command-prefix
+                             input))))
+             (fn (symbol-function
+                  (intern (concat weechat-alias-function-prefix
+                                  command)))))
+        (if (functionp fn)
+            (funcall fn input)
+          input))
+    input))
+
+(add-hook 'weechat-message-filter-functions #'weechat-alias-apply)
+
+(provide 'weechat-alias)
+
+;;; weechat-alias.el ends here
