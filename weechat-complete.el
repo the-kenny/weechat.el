08362a9be851adebe6a359047df6cbf1468b0d84
Moritz Ulrich <moritz@tarn-vedra.de>
2015-03-21 20:23:44 +0100

    Also downcase usernames for completion.

--- weechat-complete.el
+++ weechat-complete.el
@@ -110,10 +110,10 @@ Copied from `pcomplete-erc-command-name'."
 (defun pcomplete/weechat/complete-command ()
   "Complete the initial command argument."
   (pcomplete-here
-   (append
-    (pcomplete-weechat-commands)
-    (mapcar 'downcase (pcomplete-weechat-commands))
-    (pcomplete-weechat-nicks weechat-complete-nick-postfix weechat-complete-nick-ignore-self))))
+   (mapcar 'downcase
+           (append
+            (pcomplete-weechat-commands)
+            (pcomplete-weechat-nicks weechat-complete-nick-postfix weechat-complete-nick-ignore-self)))))
 
 (defun pcomplete/weechat-mode/WHOIS ()
   (pcomplete-here (pcomplete-weechat-all-nicks)))
